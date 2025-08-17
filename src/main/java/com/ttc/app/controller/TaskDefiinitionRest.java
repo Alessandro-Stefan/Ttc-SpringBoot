df7810092a4e1597f4f3a36c623b554c7bc46758
Alessandro-Stefan <alessandrostefan2@gmail.com>
2025-08-17 22:25:20 +0200

    [feature/DEV-03] Ttc-Springboot - Implementazione mapper TaskDefinition

---
+++ src/main/java/com/ttc/app/controller/TaskDefiinitionRest.java
@@ -0,0 +1,11 @@
+package com.ttc.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class TaskDefiinitionRest {
+
+}
